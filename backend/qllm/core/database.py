8f3b1ae7365d78ff1a38f12991e38a43a4075304
LongBaoCoder2 <longbao.it@gmail.com>
2025-02-20 11:35:30 +0700

    config: async engine driver

--- backend/qllm/core/database.py
+++ backend/qllm/core/database.py
@@ -4,6 +4,9 @@ from qllm.core.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
+if "postgresql://" in DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+
 print("DATABASE_URL: ", DATABASE_URL)
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
