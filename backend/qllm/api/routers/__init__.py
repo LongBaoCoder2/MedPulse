c1c4857d10696ee9312934146bc482ef0e4dd0d8
LongBaoCoder2 <longbao.it@gmail.com>
2025-02-20 12:16:59 +0700

    feat: add healthcheck route

--- backend/qllm/api/routers/__init__.py
+++ backend/qllm/api/routers/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from .auth import auth_router
 from .conversation import conversation_route
 from .upload import upload_router
+from .health import health_check_router
 
 api_router = APIRouter()
 api_router.include_router(router=auth_router, prefix="/auth")
 api_router.include_router(router=upload_router, prefix="/upload")
 api_router.include_router(router=conversation_route, prefix="/chat")
+api_router.include_router(router=health_check_router, prefix="/heath")
